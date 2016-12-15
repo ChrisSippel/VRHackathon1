878fc5565b05141976dbf2ce8fb633554209b474
Hayden Taylor <hayden.taylor@magnetforensics.com>
2016-12-15 15:57:29 -0500

    Hayden: Functionality on Menu
    - added proper functionality for menu buttons

---
+++ VRHackathon1/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public string levelName = "Test Menu";
+
+    public void SwitchLevel()
+    {
+        if (levelName != "Quit")
+        {
+            SceneManager.LoadScene(levelName);
+        }
+        else
+        {
+            Application.Quit();
+        }               
+    }
+}
---
+++ VRHackathon1/Assets/Scripts/RayViewer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour
+{
+
+    public float weaponRange = 50f;
+
+    private Camera fpsCam;
+
+    // This is debug code, don't put in final release
+	void Start () {
+        fpsCam = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange);
+	}
+}
